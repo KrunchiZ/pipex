8232c02e55504a568df014266623fefafd6b1023
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-07-31 13:08:24 +0800

    fixed prototype norm error

--- px_exec_pipex.c
+++ px_exec_pipex.c
@@ -6,14 +6,14 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 14:29:49 by kchiang           #+#    #+#             */
-/*   Updated: 2025/07/31 00:51:29 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/07/31 13:07:55 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	px_exec_child_process(t_uchar append_mode, char **argv
-	, int *pipefd, int input_fd);
+static void	px_exec_child_process(t_uchar append_mode, char **argv,
+				int *pipefd, int input_fd);
 
 /* External Variable accessing the Environment config in the terminal.
  * Similar to envp from int main(int argc, char **argv, char **envp){}.
@@ -50,12 +50,12 @@ void	px_exec_pipex(t_uchar append_mode, char **argv, int cmd_count
 static void	px_exec_child_process(t_uchar append_mode, char **argv
 	, int *pipefd, int input_fd)
 {
-	int	out_fd;
+	int		out_fd;
+	char	**cmd;
 
-	close(pipefd[0]);
 	if (dup2(input_f d, STDIN_FILENO) == -1)
 		px_perror_exit("dup2");
-	close(input_fd);
+	close(input_fd) + close(pipefd[0]);
 	if (cmd_count > 1)
 	{
 		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
@@ -64,7 +64,6 @@ static void	px_exec_child_process(t_uchar append_mode, char **argv
 	}
 	else
 	{
-		close(pipefd[1]);
 		if (append_mode == true)
 			out_fd = open(argv[1], O_WRONLY, O_CREAT, O_APPEND);
 		else
@@ -73,7 +72,7 @@ static void	px_exec_child_process(t_uchar append_mode, char **argv
 			px_perror_exit("open");
 		if (dup2(out_fd, STDOUT_FILENO) == -1)
 			px_perror_exit("dup2");
-		close(out_fd);
+		close(out_fd) + close(pipefd[1]);
 	}
 	/* ft_split argv[0] before parsing to execve.
 	 * split[0] for first arg, whole split for second arg.
