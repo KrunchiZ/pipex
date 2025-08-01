2c24a2ecbc7fe152de62068763778e3bfead9012
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-08-01 12:58:56 +0800

    added specific error messages for empty strings and separated command flags

--- src/px_main.c
+++ src/px_main.c
@@ -6,20 +6,21 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 18:35:16 by kchiang           #+#    #+#             */
-/*   Updated: 2025/08/01 12:16:10 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/08/01 12:57:09 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	px_file_valid_check(int argc, char **argv);
+static void	px_arg_check(int argc, char **argv);
+static void	px_valid_check(int argc, char **argv);
 
 int	main(int argc, char **argv, char **envp)
 {
 	int		input_fd;
 	t_vars	vars;
 
-	px_file_valid_check(argc, argv);
+	px_arg_check(argc, argv);
 	vars.envp = envp;
 	vars.append_mode = false;
 	px_init_input_fd(&input_fd, argv, &vars);
@@ -33,10 +34,8 @@ int	main(int argc, char **argv, char **envp)
 	return (EXIT_SUCCESS);
 }
 
-static void	px_file_valid_check(int argc, char **argv)
+static void	px_arg_check(int argc, char **argv)
 {
-	int	i;
-
 	if (ft_strncmp(argv[1], HERE_DOC, 8))
 	{
 		if (argc < 5)
@@ -51,11 +50,21 @@ static void	px_file_valid_check(int argc, char **argv)
 		if (access(argv[argc - 1], F_OK) == 0 && access(argv[argc - 1], W_OK) == -1)
 			px_perror_exit(argv[argc - 1]);
 	}
+	px_valid_check(argc, argv);
+	return ;
+}
+
+static void	px_valid_check(int argc, char **argv)
+{
+	int	i;
+
 	i = 1;
 	while (i < argc)
 	{
-		if (*(argv[i]) == '\0' || *(argv[i]) == '-')
-			px_error_abort("error: Invalid arguments.");
+		if (*(argv[i]) == '\0')
+			px_error_abort("error: Empty string detected.");
+		if (*(argv[i]) == '-')
+			px_error_abort("error: Command & flags not quoted together.");
 		i++;
 	}
 	return ;
