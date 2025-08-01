9a22d6a4e218008b66735d8d3f8b95bab842250b
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-08-01 11:40:35 +0800

    handled empty string arguments

--- src/px_main.c
+++ src/px_main.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 18:35:16 by kchiang           #+#    #+#             */
-/*   Updated: 2025/08/01 02:35:15 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/08/01 11:39:14 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,8 @@ int	main(int argc, char **argv, char **envp)
 
 static void	px_file_valid_check(int argc, char **argv)
 {
+	int	i;
+
 	if (ft_strncmp(argv[1], HERE_DOC, 8))
 	{
 		if (argc < 5)
@@ -46,5 +48,11 @@ static void	px_file_valid_check(int argc, char **argv)
 		px_error_abort("error: Invalid arguments.");
 	if (access(argv[argc - 1], F_OK) == 0 && access(argv[argc - 1], W_OK) == -1)
 		px_perror_exit(argv[argc - 1]);
+	i = 1;
+	while (i < argc)
+	{
+		if (argv[i++][0] == '\0')
+			px_error_abort("error: Invalid arguments.");
+	}
 	return ;
 }
