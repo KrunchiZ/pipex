289d45799f3d7ccc18a908ffaebb836936cab196
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-08-05 11:09:12 +0800

    fixed naming consistency in get_path

--- src/px_get_path.c
+++ src/px_get_path.c
@@ -6,19 +6,19 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 16:24:33 by kchiang           #+#    #+#             */
-/*   Updated: 2025/08/02 21:49:34 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/08/05 11:08:55 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
 static char	**px_parse_path_env(char **envp, char **cmd, char *cmd_path);
-static char	*px_get_next_path(char **env, char *cmd_path, char **cmd);
+static char	*px_get_next_path(char **paths, char *cmd_path, char **cmd);
 static char	*px_join_path(char **paths, char *path, char *cmd_path, char **cmd);
 
 char	*px_get_path(char **cmd, char **envp)
 {
-	char	**paths;
+	char	**env_paths;
 	char	*cmd_path;
 
 	cmd_path = ft_strjoin("./", cmd[0]);
@@ -29,14 +29,14 @@ char	*px_get_path(char **cmd, char **envp)
 	}
 	if (access(cmd_path, X_OK) == 0)
 		return (cmd_path);
-	paths = px_parse_path_env(envp, cmd, cmd_path);
-	return (px_get_next_path(paths, cmd_path, cmd));
+	env_paths = px_parse_path_env(envp, cmd, cmd_path);
+	return (px_get_next_path(env_paths, cmd_path, cmd));
 }
 
 static char	**px_parse_path_env(char **envp, char **cmd, char *cmd_path)
 {
 	char	*env_var;
-	char	**paths;
+	char	**env_paths;
 
 	while (*envp)
 	{
@@ -44,14 +44,14 @@ static char	**px_parse_path_env(char **envp, char **cmd, char *cmd_path)
 		if (!ft_strncmp(env_var, "PATH=", 5))
 			break ;
 	}
-	paths = ft_split(env_var, "=:");
-	if (!paths)
+	env_paths = ft_split(env_var, "=:");
+	if (!env_paths)
 	{
 		free(cmd_path);
 		px_free_arg(cmd);
 		px_error_abort("error: ft_split failed.");
 	}
-	return (paths);
+	return (env_paths);
 }
 
 static char	*px_get_next_path(char **paths, char *cmd_path, char **cmd)
