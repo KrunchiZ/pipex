82e875a6c21fb3b1d9167587b0a60a33d777cccd
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-08-12 18:37:21 +0800

    updated px_split to skip copying the starting and ending quote of an arg

--- src/px_split.c
+++ src/px_split.c
@@ -1,20 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   px_split.c                                         :+:      :+:    :+:   */
+/*   px_split_bonus.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 09:19:41 by kchiang           #+#    #+#             */
-/*   Updated: 2025/08/02 13:09:09 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/08/12 18:35:47 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "pipex_bonus.h"
 
 static void	px_count_arg(const char *s, const char *set, int *count);
 static int	px_fillhost(char **host, const char *s, const char *set, int count);
 static void	px_arglen(const char *s, const char *set, int *len);
+static void	px_free_host(char **host, int depth);
 
 char	**px_split(const char *s, const char *set)
 {
@@ -77,15 +78,15 @@ static int	px_fillhost(char **host, const char *s, const char *set, int count)
 			&& (i == 0 || (i > 0 && ft_strchr(set, s[i - 1]))))
 		{
 			px_arglen(&s[i], set, &arglen);
+			if (s[i] == '\'' || s[i] == '\"')
+			{
+				i++;
+				arglen -= 2;
+			}
 			host[depth] = malloc(arglen + 1);
 			if (host == NULL)
-			{
-				while (depth >= 0)
-					free(host[depth--]);
-				return (free(host), false);
-			}
-			ft_strlcpy(host[depth], &s[i], arglen + 1);
-			depth++;
+				return (px_free_host(host, depth), false);
+			ft_strlcpy(host[depth++], &s[i], arglen + 1);
 		}
 		i++;
 	}
@@ -118,5 +119,13 @@ static void	px_arglen(const char *s, const char *set, int *len)
 			s++;
 		}
 	}
+	return ;
+}
+
+static void	px_free_host(char **host, int depth)
+{
+	while (depth >= 0)
+		free(host[depth--]);
+	free(host);
 	return ;
 }
