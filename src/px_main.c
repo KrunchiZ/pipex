ecf492d54bb1fd04f25dbbb8421aa98696a6f82e
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-08-20 01:50:38 +0800

    Renamed arg_check in main to init_filefd. Reorganized if else block.

--- src/px_main.c
+++ src/px_main.c
@@ -6,13 +6,13 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 18:35:16 by kchiang           #+#    #+#             */
-/*   Updated: 2025/08/20 01:34:22 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/08/20 01:42:00 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	px_arg_check(int argc, char **argv, t_vars *vars);
+static void	px_init_filefd(int argc, char **argv, t_vars *vars);
 static void	px_open_outfile(t_vars *vars, char *file);
 
 int	main(int argc, char **argv, char **envp)
@@ -22,7 +22,7 @@ int	main(int argc, char **argv, char **envp)
 	vars.envp = envp;
 	vars.infile = argv[1];
 	vars.cmd_count = argc - 3;
-	px_arg_check(argc, argv, &vars);
+	px_init_filefd(argc, argv, &vars);
 	if (vars.append_mode)
 		px_exec_pipex(vars, argv + 3);
 	else
@@ -30,22 +30,22 @@ int	main(int argc, char **argv, char **envp)
 	return (EXIT_SUCCESS);
 }
 
-static void	px_arg_check(int argc, char **argv, t_vars *vars)
+static void	px_init_filefd(int argc, char **argv, t_vars *vars)
 {
-	if (!px_input_is_heredoc(vars->infile))
-	{
-		vars->append_mode = false;
-		if (argc < 5)
-			px_error_abort("pipex: Invalid arguments.");
-		px_parse_infile_fd(vars);
-	}
-	else
+	if (px_input_is_heredoc(vars->infile))
 	{
 		vars->append_mode = true;
 		if (argc < 6)
 			px_error_abort("pipex: Invalid arguments.");
 		vars->cmd_count--;
 		px_parse_heredoc_fd(argv, vars);
+	}
+	else
+	{
+		vars->append_mode = false;
+		if (argc < 5)
+			px_error_abort("pipex: Invalid arguments.");
+		px_parse_infile_fd(vars);
 	}
 	px_open_outfile(vars, argv[argc - 1]);
 	return ;
