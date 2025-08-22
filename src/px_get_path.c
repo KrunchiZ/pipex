571157e6c60441f90229ccac35eb333897710cec
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-08-22 15:00:02 +0800

    reimplemented px_cmd_error

--- src/px_get_path.c
+++ src/px_get_path.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 16:24:33 by kchiang           #+#    #+#             */
-/*   Updated: 2025/08/21 19:12:41 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/08/22 14:50:47 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #define IS_FIRST_CHECK true
 #define NOT_FIRST_CHECK false
 
-static int	px_path_is_valid(char *cmd_path, int is_first_check, char **cmd);
+static int	px_path_is_valid(char *cmd_path, int is_first_check);
 static char	**px_parse_path_env(char **envp, char **cmd, char *cmd_path);
 static char	*px_get_next_path(char **paths, char *cmd_path, char **cmd);
 static char	*px_join_path(char **paths, char *path, char *cmd_path, char **cmd);
@@ -24,7 +24,7 @@ char	*px_get_path(char **cmd, char **envp)
 	char	**env_paths;
 	char	*cmd_path;
 
-	if (cmd[0] && px_path_is_valid(cmd[0], IS_FIRST_CHECK, cmd))
+	if (cmd[0] && px_path_is_valid(cmd[0], IS_FIRST_CHECK))
 		return (ft_strdup(cmd[0]));
 	cmd_path = ft_strjoin("./", cmd[0]);
 	if (!cmd_path)
@@ -38,32 +38,11 @@ char	*px_get_path(char **cmd, char **envp)
 	return (px_get_next_path(env_paths, cmd_path, cmd));
 }
 
-static int	px_path_is_valid(char *cmd_path, int is_first_check, char **cmd)
+static int	px_path_is_valid(char *cmd_path, int is_first_check)
 {
-	if (access(cmd_path, F_OK) == 0)
-	{
-		if (access(cmd_path, X_OK) == 0)
-			return (true);
-		else
-		{
-			ft_putstr_fd("pipex: ", STDERR_FILENO);
-			ft_putstr_fd(cmd_path, STDERR_FILENO);
-			if (!is_first_check)
-				free(cmd_path);
-			px_free_arg(cmd);
-			ft_putstr_fd(": ", STDERR_FILENO);
-			px_error_abort(strerror(errno), PERM_DENIED);
-		}
-	}
-	if (is_first_check && (*cmd_path == '/' || !ft_strncmp(cmd_path, "./", 2)
-			|| !ft_strncmp(cmd_path, "~/", 2)))
-	{
-		ft_putstr_fd("pipex: ", STDERR_FILENO);
-		ft_putstr_fd(cmd_path, STDERR_FILENO);
-		px_free_arg(cmd);
-		ft_putstr_fd(": ", STDERR_FILENO);
-		px_error_abort(strerror(errno), NOT_FOUND);
-	}
+	if (access(cmd_path, F_OK) == 0
+		|| (is_first_check && ft_strchr(cmd_path, '/')))
+		return (true);
 	return (false);
 }
 
@@ -99,7 +78,7 @@ static char	*px_get_next_path(char **paths, char *cmd_path, char **cmd)
 	while (*cursor)
 	{
 		cmd_path = px_join_path(paths, *cursor++, cmd_path, cmd);
-		if (px_path_is_valid(cmd_path, NOT_FIRST_CHECK, cmd))
+		if (px_path_is_valid(cmd_path, NOT_FIRST_CHECK))
 			break ;
 	}
 	px_free_arg(paths);
