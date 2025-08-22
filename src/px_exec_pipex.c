5e8212584a523519c96f926812b285ba2ccda439
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-08-23 01:34:14 +0800

    added cmd NULL check. If true, ft_strdup argv

--- src/px_exec_pipex.c
+++ src/px_exec_pipex.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 14:29:49 by kchiang           #+#    #+#             */
-/*   Updated: 2025/08/22 14:47:47 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/08/23 01:31:21 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 static void	px_parent_process(t_vars *vars, char ***argv, int *current);
 static void	px_child_process(t_vars *vars, char **argv, int current);
+static char	**px_init_cmd(char **argv);
 static void	px_init_output(t_vars *vars, int current);
-static void	px_dup_filefd(t_vars *vars);
 
 /* Piping function that recurses itself.
  * */
@@ -70,9 +70,7 @@ static void	px_child_process(t_vars *vars, char **argv, int current)
 		px_perror_exit("pipex: dup2", EXIT_FAILURE);
 	close(vars->input_fd);
 	px_init_output(vars, current);
-	cmd = px_split(*argv, WHITESPACE);
-	if (!cmd)
-		px_error_abort("pipex: px_split failed", EXIT_FAILURE);
+	cmd = px_init_cmd(argv);
 	execpath = px_get_path(cmd, vars->envp);
 	if (!execpath)
 		px_free_exit(cmd, "pipex: ft_strdup failed", EXIT_FAILURE);
@@ -80,10 +78,38 @@ static void	px_child_process(t_vars *vars, char **argv, int current)
 	px_cmd_error(execpath, cmd);
 }
 
+static char	**px_init_cmd(char **argv)
+{
+	char	**cmd;
+
+	cmd = px_split(*argv, WHITESPACE);
+	if (!cmd)
+		px_error_abort("pipex: px_split failed", EXIT_FAILURE);
+	if (!*cmd)
+	{
+		free(cmd);
+		cmd = malloc(2 * sizeof(char *));
+		if (!cmd)
+			px_error_abort("pipex: malloc failed", EXIT_FAILURE);
+		cmd[1] = NULL;
+		cmd[0] = ft_strdup(*argv);
+		if (!cmd[0])
+			px_error_abort("pipex: ft_strdup failed", EXIT_FAILURE);
+	}
+	return (cmd);
+}
+
 static void	px_init_output(t_vars *vars, int current)
 {
 	if (current == vars->cmd_count - 1)
-		px_dup_filefd(vars);
+	{
+		close(vars->pipefd[1]);
+		if (vars->outfd == -1)
+			exit(EXIT_FAILURE);
+		if (dup2(vars->outfd, STDOUT_FILENO) == -1)
+			px_perror_exit("pipex: dup2", EXIT_FAILURE);
+		close(vars->outfd);
+	}
 	else
 	{
 		close(vars->outfd);
@@ -91,16 +117,5 @@ static void	px_init_output(t_vars *vars, int current)
 			px_perror_exit("pipex: dup2", EXIT_FAILURE);
 		close(vars->pipefd[1]);
 	}
-	return ;
-}
-
-static void	px_dup_filefd(t_vars *vars)
-{
-	close(vars->pipefd[1]);
-	if (vars->outfd == -1)
-		exit(EXIT_FAILURE);
-	if (dup2(vars->outfd, STDOUT_FILENO) == -1)
-		px_perror_exit("pipex: dup2", EXIT_FAILURE);
-	close(vars->outfd);
 	return ;
 }
