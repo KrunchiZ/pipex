a7c647dce8ea7c6560a46efd0b916282f542d241
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-08-23 21:51:47 +0800

    reduced redundancy in get_path

--- src/px_get_path.c
+++ src/px_get_path.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 16:24:33 by kchiang           #+#    #+#             */
-/*   Updated: 2025/08/22 14:50:47 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/08/23 19:08:57 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,29 @@
 #define NOT_FIRST_CHECK false
 
 static int	px_path_is_valid(char *cmd_path, int is_first_check);
-static char	**px_parse_path_env(char **envp, char **cmd, char *cmd_path);
-static char	*px_get_next_path(char **paths, char *cmd_path, char **cmd);
-static char	*px_join_path(char **paths, char *path, char *cmd_path, char **cmd);
+static char	**px_parse_path_env(char **envp, char **cmd);
+static char	*px_join_path(char **env_paths, char *path, char **cmd);
 
 char	*px_get_path(char **cmd, char **envp)
 {
 	char	**env_paths;
 	char	*cmd_path;
+	char	**cursor;
 
 	if (cmd[0] && px_path_is_valid(cmd[0], IS_FIRST_CHECK))
 		return (ft_strdup(cmd[0]));
-	cmd_path = ft_strjoin("./", cmd[0]);
-	if (!cmd_path)
-	{
-		px_free_arg(cmd);
-		px_error_abort("pipex: ft_strjoin", EXIT_FAILURE);
-	}
-	env_paths = px_parse_path_env(envp, cmd, cmd_path);
+	env_paths = px_parse_path_env(envp, cmd);
 	if (!env_paths)
-		return (cmd_path);
-	return (px_get_next_path(env_paths, cmd_path, cmd));
+		return (ft_strdup(cmd[0]));
+	cursor = env_paths;
+	while (*cursor)
+	{
+		cmd_path = px_join_path(env_paths, *cursor++, cmd);
+		if (px_path_is_valid(cmd_path, NOT_FIRST_CHECK))
+			break ;
+	}
+	px_free_arg(env_paths);
+	return (cmd_path);
 }
 
 static int	px_path_is_valid(char *cmd_path, int is_first_check)
@@ -46,7 +48,7 @@ static int	px_path_is_valid(char *cmd_path, int is_first_check)
 	return (false);
 }
 
-static char	**px_parse_path_env(char **envp, char **cmd, char *cmd_path)
+static char	**px_parse_path_env(char **envp, char **cmd)
 {
 	char	*env_var;
 	char	**env_paths;
@@ -63,38 +65,22 @@ static char	**px_parse_path_env(char **envp, char **cmd, char *cmd_path)
 	env_paths = ft_split(env_var, "=:");
 	if (!env_paths)
 	{
-		free(cmd_path);
 		px_free_arg(cmd);
 		px_error_abort("pipex: ft_split: malloc failed", EXIT_FAILURE);
 	}
 	return (env_paths);
 }
 
-static char	*px_get_next_path(char **paths, char *cmd_path, char **cmd)
-{
-	char	**cursor;
-
-	cursor = paths;
-	while (*cursor)
-	{
-		cmd_path = px_join_path(paths, *cursor++, cmd_path, cmd);
-		if (px_path_is_valid(cmd_path, NOT_FIRST_CHECK))
-			break ;
-	}
-	px_free_arg(paths);
-	return (cmd_path);
-}
-
-static char	*px_join_path(char **paths, char *path, char *cmd_path, char **cmd)
+static char	*px_join_path(char **env_paths, char *path, char **cmd)
 {
 	char	*tmp;
+	char	*cmd_path;
 
-	free(cmd_path);
 	tmp = ft_strjoin(path, "/");
 	if (!tmp)
 	{
 		px_free_arg(cmd);
-		px_free_arg(paths);
+		px_free_arg(env_paths);
 		px_error_abort("pipex: ft_strjoin: path + '/' failed", EXIT_FAILURE);
 	}
 	cmd_path = ft_strjoin(tmp, cmd[0]);
@@ -102,7 +88,7 @@ static char	*px_join_path(char **paths, char *path, char *cmd_path, char **cmd)
 	{
 		free(tmp);
 		px_free_arg(cmd);
-		px_free_arg(paths);
+		px_free_arg(env_paths);
 		px_error_abort("pipex: ft_strjoin: path + cmd failed", EXIT_FAILURE);
 	}
 	free(tmp);
